16cb493b912b2eb1bd7da91313632ec20e69f7dc
Jim <james@lendable.io>
2015-08-10 00:23:52 +1000

    Version that calculates RMSE over all of history

---
+++ Run_multiple_sims.R
@@ -0,0 +1,106 @@
+
+# Read in libraries and data ----------------------------------------------
+library(randomForest); library(shinystan);library(vars); library(zoo); library(rstan);  library(ggplot2); library(magrittr); library(lubridate); library(stringr); library(reshape2); library(parallel);  library(dplyr); 
+cc <- function(x) x[complete.cases(x),]
+
+CPI <- read.csv("./Data/CPIAUCSL.csv")
+UNEMP <- read.csv("./Data/UNRATE.csv")
+TREAS <- read.csv("./Data/TB3MS.csv")
+
+converter <- function(x, value){
+  x$Date <- as.Date(x$DATE)
+  x$DATE <- NULL
+  x[,value] <- x$VALUE
+  x$VALUE <- NULL
+  x
+}
+
+CPI %<>% converter("CPI")
+UNEMP %<>% converter("Unemp")
+TREAS %<>% converter("r")
+dataset <- left_join(UNEMP, CPI) %>% left_join(TREAS) %>% cc
+
+dataset.q <- dataset %>% group_by(quarter = as.yearqtr(Date) %>% as.Date) %>%
+  summarise(Unemp = first(Unemp),
+            CPI = first(CPI),
+            r = mean(r)) %>%
+  mutate(CPI = c(NA, diff(log(CPI))*100)) %>% cc %>% filter(quarter > "1960-01-01")
+
+cpier <- function(x){
+  out <- rep(NA, length(x))
+  for(i in 4:length(x)){
+    out[i] <- prod(1 + x[(i-3):i]/100)*100-100
+  }
+  out
+}
+dataset.q <- dataset.q %>% mutate(CPI = cpier(CPI)) %>% cc
+
+enddates <- seq(from = as.Date("1970-01-01"), to = as.Date("2014-01-01"), by = "year")
+
+out <- data.frame(quarter = enddates) %>% left_join(dataset.q)
+out2 <- data.frame(quarter = enddates, matrix(NA, length(enddates), 6))
+names(out2)[-1] <- paste0(names(dataset.q)[-1], rep(1:2, each = 3))
+out <- left_join(out, out2)
+
+
+
+for(kk in enddates){
+dat1 <- dataset.q[dataset.q$quarter<=kk,-1]
+
+# Run the random forests to get the proximity matrix weights
+rf1 <- randomForest(Unemp ~ ., data = dat1, proximity = T, ntree = 5000, nodesize = 40, oob.prox = T)
+rf2 <- randomForest(CPI ~ ., data = dat1, proximity = T, ntree = 5000, nodesize = 40, oob.prox = T)
+rf3 <- randomForest(r ~ ., data = dat1, proximity = T, ntree = 5000, nodesize = 40, oob.prox = T)
+proxmat <- (rf1$proximity + rf2$proximity + rf3$proximity)/3
+proxmat[upper.tri(proxmat, diag  = T)] <- 0
+
+weights <- data.frame(w = proxmat[nrow(proxmat)-3,], d = dataset.q$quarter[dataset.q$quarter<=kk])
+
+# Generate priors for VARs ---------------------------------------------------------------
+
+vm.int <- VAR(dat1, p = 2, type = "const")
+int_prior <- do.call(rbind, lapply(coef(vm.int), function(x) x[7,1]))
+firstlag <- do.call(rbind, lapply(coef(vm.int), function(x) x[1:3,1]))
+secondlag <- do.call(rbind, lapply(coef(vm.int), function(x) x[4:6,1]))
+
+
+# New rewrite -------------------------------------------------------------
+P <- ncol(dat1); T <- nrow(dat1)
+weights2 <- rep(1, nrow(dat1))
+weights2[(length(weights2)-3):length(weights2)] <- 0
+model.data2 <- list(Y = dat1, linear_est = as.vector(m4), 
+                    linear_est_1 = as.vector(firstlag),
+                    linear_est_2 = as.vector(secondlag),
+                    weights = weights$w,
+                    weights2 = weights2,
+                    nu_var = 1.4,
+                    P = P, T = T,
+                    linear_int = linearint)
+
+# hold on to your butts
+
+tvp_mod <- stan(file = "analogy_weighted_var2.cpp", data = model.data2, chains = 1, iter = 50)
+
+tvp_mc <- mclapply(1:3, mc.cores = 3, function(i){
+  stan(fit = tvp_mod, data = model.data2, chains =1, chain_id = i, verbose = TRUE, refresh = -1, 
+       warmup = 200, iter = 400)
+})
+
+
+tvp_mc1 <- sflist2stanfit(tvp_mc)
+#launch_shinystan(tvp_mc1)
+
+
+yhat <- rstan::extract(tvp_mc1, pars = "Yhat", permuted= FALSE)
+yhat2 <- rstan::extract(tvp_mc1, pars = "Yhat2", permuted= FALSE)
+yhat <- do.call(rbind, lapply(1:3, function(i) yhat[,i,])) %>% as.data.frame
+yhat2 <- do.call(rbind, lapply(1:3, function(i) yhat2[,i,])) %>% as.data.frame
+
+yhat.mean <- colMeans(yhat)
+names(yhat.mean) <- NULL
+yhat2.mean  <- colMeans(yhat2)
+names(yhat2.mean) <- NULL
+out[out$quarter==kk,5:7] <- yhat.mean
+out[out$quarter==kk,8:10] <- yhat2.mean
+}
+
---
+++ analogy_weighted_var2.cpp
@@ -0,0 +1,81 @@
+data {
+  // This version has a generated quantities block to generate estimates of the permanent component.
+  int T; // number of observations
+  int P; // number of variables
+  vector[P] Y[T]; // An array of P columns and T rows containing the data
+  vector[P*P] linear_est_1; // prior for theta_1
+  vector[P*P] linear_est_2; // prior for theta_2
+  vector[P] linear_int; // prior for the intercept
+  vector[T] weights; // The analogy weights
+  vector[T] weights2; // weights for the regular (unweighted) VAR (a vector of 1s with zeros for the last 4 observations)
+  real nu_var; // The degrees of freedom paramter for the LKJ prior on the residual correlation matrix
+  }
+parameters {
+  vector[P] intercept;
+  matrix[P,P] theta_1; 
+  matrix[P,P] theta_2; 
+  corr_matrix[P] Omega_VAR;
+  vector<lower= 0>[P] tau_VAR;
+  vector[P] intercept2;
+  matrix[P,P] theta_12; 
+  matrix[P,P] theta_22; 
+  corr_matrix[P] Omega_VAR2;
+  vector<lower= 0>[P] tau_VAR2;
+}
+transformed parameters{
+  matrix[P,P] Sig_VAR;
+  matrix[P,P] Sig_VAR2;
+  Sig_VAR2 <- quad_form_diag(Omega_VAR2, tau_VAR2);
+  Sig_VAR <- quad_form_diag(Omega_VAR, tau_VAR);
+}
+model {
+  // LKJ priors for the covariance matrices
+  tau_VAR ~ cauchy(0,2.5);
+  Omega_VAR ~ lkj_corr(nu_var);
+  tau_VAR2 ~ cauchy(0,2.5);
+  Omega_VAR2 ~ lkj_corr(nu_var);
+  
+  // prior for the regression coeffiecients
+  //intercept ~ multi_normal(linear_int, Sig_int);
+  intercept ~ normal(0, 1);
+  to_vector(theta_1) ~ normal(linear_est_1, 0.3);
+  to_vector(theta_2) ~ normal(linear_est_2, 0.3);
+  intercept2 ~ normal(0, 1);
+  to_vector(theta_12) ~ normal(linear_est_1, 0.3);
+  to_vector(theta_22) ~ normal(linear_est_2, 0.3);
+  
+  for(t in 3:T){
+    increment_log_prob(weights2[t]*multi_normal_log(Y[t], intercept2 + theta_12*Y[t-1] + theta_22*Y[t-2], Sig_VAR2));
+    increment_log_prob(weights[t]*multi_normal_log(Y[t], intercept + theta_1*Y[t-1] + theta_2*Y[t-2], Sig_VAR));
+  }
+}
+generated quantities{
+  vector[P] Yhat; // The one-year ahead forecasts for the weighted model
+  vector[P] Yhat2; // The one-year ahead forecasts for the unweighted model
+  {vector[P] Ytemp[6]; // 
+  vector[P] Ytemp2[6]; // 
+  int count;
+  int count2;
+  count <- 0;
+  for(t1 in (T-5):(T-4)){
+    count <- count + 1;
+    Ytemp[count] <- Y[t1];
+    Ytemp2[count] <- Y[t1];
+  }
+  // Generate forecasts for one-year ahead projections
+  count2 <- 2;
+  for(t in (T-3):T){
+    count2 <- count2 + 1;
+    Ytemp[count2] <- multi_normal_rng(intercept + theta_1*Ytemp[count2-1] + theta_2*Ytemp[count2-2], Sig_VAR);
+    Ytemp2[count2] <- multi_normal_rng(intercept2 + theta_12*Ytemp2[count2-1] + theta_22*Ytemp2[count2-2], Sig_VAR2);
+    if(t == T){
+      Yhat <- Ytemp[count2];
+      Yhat2 <- Ytemp2[count2];
+    }
+  }}
+  /*for(i in 1:P){
+    Yhat[i] <- fabs(Yhat[i] - Y[T][i]);
+    Yhat2[i] <- fabs(Yhat2[i] - Y[T][i]);
+  }*/
+  
+}
