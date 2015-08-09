ff1bc5bea36dd1e4d95a1fae48790255ea9f5c12
Jim <james@lendable.io>
2015-08-10 09:39:55 +1000

    Updates with rolling one-year-ahead forecasts

--- Run_multiple_sims.R
+++ Run_multiple_sims.R
@@ -35,7 +35,7 @@ cpier <- function(x){
 }
 dataset.q <- dataset.q %>% mutate(CPI = cpier(CPI)) %>% cc
 
-enddates <- seq(from = as.Date("1970-01-01"), to = as.Date("2014-01-01"), by = "year")
+enddates <- seq(from = as.Date("1999-01-01"), to = as.Date("2014-01-01"), by = "year")
 
 out <- data.frame(quarter = enddates) %>% left_join(dataset.q)
 out2 <- data.frame(quarter = enddates, matrix(NA, length(enddates), 6))
@@ -46,7 +46,7 @@ out <- left_join(out, out2)
 
 for(kk in enddates){
 dat1 <- dataset.q[dataset.q$quarter<=kk,-1]
-
+print(kk)
 # Run the random forests to get the proximity matrix weights
 rf1 <- randomForest(Unemp ~ ., data = dat1, proximity = T, ntree = 5000, nodesize = 40, oob.prox = T)
 rf2 <- randomForest(CPI ~ ., data = dat1, proximity = T, ntree = 5000, nodesize = 40, oob.prox = T)
@@ -68,18 +68,20 @@ secondlag <- do.call(rbind, lapply(coef(vm.int), function(x) x[4:6,1]))
 P <- ncol(dat1); T <- nrow(dat1)
 weights2 <- rep(1, nrow(dat1))
 weights2[(length(weights2)-3):length(weights2)] <- 0
-model.data2 <- list(Y = dat1, linear_est = as.vector(m4), 
+model.data2 <- list(Y = dat1, 
                     linear_est_1 = as.vector(firstlag),
                     linear_est_2 = as.vector(secondlag),
                     weights = weights$w,
                     weights2 = weights2,
                     nu_var = 1.4,
                     P = P, T = T,
-                    linear_int = linearint)
+                    linear_int = int_prior %>% as.numeric)
 
 # hold on to your butts
 
-tvp_mod <- stan(file = "analogy_weighted_var2.cpp", data = model.data2, chains = 1, iter = 50)
+if(!exists("tvp_mod")){
+  tvp_mod <- stan(file = "analogy_weighted_var2.cpp", data = model.data2, chains = 1, iter = 50)
+}
 
 tvp_mc <- mclapply(1:3, mc.cores = 3, function(i){
   stan(fit = tvp_mod, data = model.data2, chains =1, chain_id = i, verbose = TRUE, refresh = -1, 
@@ -96,11 +98,28 @@ yhat2 <- rstan::extract(tvp_mc1, pars = "Yhat2", permuted= FALSE)
 yhat <- do.call(rbind, lapply(1:3, function(i) yhat[,i,])) %>% as.data.frame
 yhat2 <- do.call(rbind, lapply(1:3, function(i) yhat2[,i,])) %>% as.data.frame
 
-yhat.mean <- colMeans(yhat)
+yhat.mean <- apply(yhat, 2, median)
 names(yhat.mean) <- NULL
-yhat2.mean  <- colMeans(yhat2)
+yhat2.mean  <- apply(yhat2, 2, median)
 names(yhat2.mean) <- NULL
 out[out$quarter==kk,5:7] <- yhat.mean
 out[out$quarter==kk,8:10] <- yhat2.mean
 }
 
+
+out2 <- out
+
+out2[,5:7] <- out2[,5:7]- out[,2:4]
+out2[,8:10] <- out2[,8:10]- out[,2:4]
+out2 <- out2[out2$quarter!=as.Date("2001-01-01"),]
+out3 <- apply(out2[,5:10], 2, abs)
+
+apply(out3, 2, function(x) sqrt(mean(x^2)))
+library(stringr)
+
+out3 %>% melt %>%
+  mutate(Variable = str_extract(Var2, "[A-Za-z]*"),
+         Type = str_extract(Var2, "[0-9]")) %>%
+  ggplot(aes(x = Var1, y = value, group = Var2, colour = Type)) + 
+  geom_line() + 
+  facet_grid(Variable~.)
