3565b8c56667394cfe49d3c18551a55cbb75af67
James Savage <jamessavage@Jim.home>
2015-10-03 22:52:32 +1000

    CCC Garch model working very nicely

---
+++ CCC_GARCH2.stan
@@ -0,0 +1,54 @@
+
+data {
+  int T; // number of observations
+  int P; // number of stocks
+  vector[P] r[T]; // matrix of log returns
+  vector<lower = 0.01>[T] weights; // observation weights
+  real df; // lkj prior degrees of freedom. A large value will induce correlation during unprecedented episodes
+  vector[P] tau1; //starting value of sd of errors
+} 
+parameters {
+  
+  corr_matrix[P] Omega; // the static correlations
+  vector[P] mu; // The mean returns vector
+  vector<lower = 0, upper = 1>[P] A; // the coefficient on lagged r in the GARCH model
+  real<lower = 0, upper = 1-A[1]> B1; // the AR coefficient in the GARCH model
+  real<lower = 0, upper = 1-A[2]> B2; // the AR coefficient in the GARCH model
+  real<lower = 0, upper = 1-A[3]> B3; // the AR coefficient in the GARCH model
+  real<lower = 0, upper = 1-A[4]> B4; // the AR coefficient in the GARCH model
+  real<lower = 0, upper = 1-A[5]> B5; // the AR coefficient in the GARCH model
+  real<lower = 0, upper = 1-A[6]> B6; // the AR coefficient in the GARCH model
+  vector<lower = 0>[P] c; // the constant term in the GARCH model
+}
+transformed parameters {
+  vector<lower = 0>[P] tau[T]; // the time-varying scale of volatility
+  vector[P] B;
+  tau[1] <- tau1;
+  
+  B[1] <- B1; B[2] <- B2; B[3] <- B3;
+  B[4] <- B4; B[5] <- B5; B[6] <- B6;
+  
+  for(p in 1:P){
+    for(tt in 2:T){
+      tau[tt][p] <- sqrt(c[p] + 
+                         A[p]*pow(r[tt-1][p] - mu[p], 2) + 
+                         B[p]*pow(tau[tt-1][p], 2));
+      
+    }
+  }
+  
+}
+model {
+  vector[P] eta[T]; // N(0,1) residuals in the measurement equation
+  // Priors
+  mu ~ normal(0, 1);
+  Omega ~ lkj_corr(df);
+  c ~ normal(0,0.1);
+  A ~ normal(0,0.2);
+  B ~ normal(0,0.2);
+  
+  // Likelihood
+  for(t in 2:T){
+    r[t] ~ multi_normal(mu,quad_form_diag(Omega, tau[t]));
+  }
+}
--- run_cccgarch.R
+++ run_cccgarch.R
@@ -1,7 +1,7 @@
 
 library(Quandl); library(rstan); library(dplyr); library(reshape2); library(stringr)
 cc <- function(x) x[complete.cases(x),]
-Quandl.api_key("BrfAiD2VDaxQEw2zgEfQ")
+Quandl.api_key("your key here")
 
 aapl <- Quandl("YAHOO/AAPL", collapse = "weekly")
 ibm <- Quandl("YAHOO/IBM", collapse = "weekly")
@@ -27,61 +27,17 @@ JNJ2 <- changer(JNJ, "JNJ")
 data <- aapl2 %>% left_join(ibm2) %>% left_join(goog2) %>% left_join(ford2) %>% left_join(BoA2) %>% left_join(JNJ2) %>% cc %>% select(-Date)
 
 data <- data %>% apply(2, function(x) diff(log(x)))
+save(data, file = "6_companies.RData")
 
 plot.ts(data)
 #data1 <- data[(nrow(data)-400):nrow(data),]
-data1 <- data
+data1 <- data*100
 
-ccgarch_data <- list(T = nrow(data1), P = ncol(data1), r = data1, weights = rep(1, nrow(data1)), df = 1.2)
+ccgarch_data <- list(T = nrow(data1), P = ncol(data1), r = data1, weights = rep(1, nrow(data1)), df = 1.2, tau1 = rep(0, 6))
 
-code <- "
-data {
-int T; // number of observations
-int P; // number of stocks
-vector[P] r[T]; // matrix of log returns
-vector<lower = 0.01>[T] weights; // observation weights
-real df; // lkj prior degrees of freedom. A large value will induce correlation during unprecedented episodes
-} 
-parameters {
-vector<lower = 0.01>[P] tau[T]; // the time-varying scale of volatility
-corr_matrix[P] Omega; // the static correlations
-vector[P] mu; // The mean returns vector
-vector[P] A; // the coefficient on lagged r in the GARCH model
-vector[P] B; // the AR coefficient in the GARCH model
-vector[P] c; // the constant term in the GARCH model
-corr_matrix[P] Omega_nu; // The covariance matrix on the innovations in the state
-vector<lower = 0.01>[P] tau_nu; // the scale of errors in innovations to the state
-}
-transformed parameters {
-cov_matrix[P] Sig_nu; // The covariance matrix of innovations in the state
-matrix[P,P] L; // 
-Sig_nu <- quad_form_diag(Omega_nu, tau_nu); // generate 
-L <- cholesky_decompose(Sig_nu); // The Cholesky decompostion of the covariance matrix
-}
-model {
-vector[P] eta[T]; // N(0,1) residuals in the measurement equation
-vector[P] nu[T]; // N(0,1) residuals in the state equation
-// Priors
-mu ~ normal(0, 0.5);
-Omega ~ lkj_corr(df);
-c ~ normal(0,1);
-A ~ normal(0,1);
-B ~ normal(0,1);
-for(i in 1:T){
-tau[i] ~ cauchy(0.1, 2.5);
-}
-tau_nu ~ cauchy(0.1, 2.5);
+ccgarch1 <- stan(file = "~/Documents/Thesis_work/CCC_GARCH2.stan", data = ccgarch_data, iter = 10, chains = 1, control=list( adapt_delta=0.9))
+ccgarch2 <- stan(fit = ccgarch1, data = ccgarch_data, iter = 300, chains = 4, cores = 4, warmup = 150, control=list( adapt_delta=0.9))
 
-// Likelihood
-for(t in 2:T){
-eta[t] <- inverse(quad_form_diag(Omega, tau[t]))*(r[t] - mu);
-nu[t] <- inverse(L)*(tau[t] - c - A .*r[t-1] - B .*tau[t-1]);
-eta[t] ~ normal(0,1);
-nu[t] ~ normal(0,1);
-}
-}"
-ccgarch1 <- stan(file = "CCC_GARCH.cpp", data = ccgarch_data, iter = 10, chains = 1, control=list( adapt_delta=0.9))
-ccgarch2 <- stan(fit = ccgarch1, data = ccgarch_data, iter = 600, chains = 4, cores = 4, init = 0, warmup = 250, control=list( adapt_delta=0.9))
 save(ccgarch2, file = "20150929_ccgarchfit2.RData")
 library(shinystan)
 launch_shinystan(ccgarch2)
@@ -102,6 +58,6 @@ taus <- taus %>% group_by(variable) %>%
             observation = first(observation)) %>% filter(!is.na(series))
 
 
-taus %>% ggplot(aes(x = observation, colour = series, group = series, y = median)) +
+taus %>% ggplot(aes(x = observation, y = median)) +
   geom_line() +
   facet_grid(series~., scales = "free")
