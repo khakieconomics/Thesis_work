f3a6b14df218e41299d29672838dc1f077087dcc
James Savage <jamessavage@Jim.home>
2015-10-04 00:06:26 +1000

    working a treat

--- run_cccgarch.R
+++ run_cccgarch.R
@@ -33,20 +33,15 @@ plot.ts(data)
 #data1 <- data[(nrow(data)-400):nrow(data),]
 data1 <- data*100
 
-ccgarch_data <- list(T = nrow(data1), P = ncol(data1), r = data1, weights = rep(1, nrow(data1)), df = 1.2, tau1 = rep(0, 6))
+ccgarch_data <- list(T = nrow(data1), P = ncol(data1), r = data1, weights = rep(1, nrow(data1)), df = 1.2, tau1 = abs(data[1,]))
 
 ccgarch1 <- stan(file = "~/Documents/Thesis_work/CCC_GARCH2.stan", data = ccgarch_data, iter = 10, chains = 1, control=list( adapt_delta=0.9))
-ccgarch2 <- stan(fit = ccgarch1, data = ccgarch_data, iter = 300, chains = 4, cores = 4, warmup = 150, control=list( adapt_delta=0.9))
+ccgarch2 <- stan(fit = ccgarch1, data = ccgarch_data, iter = 500, chains = 4, cores = 4, warmup = 150, control=list( adapt_delta=0.9))
 
-save(ccgarch2, file = "20150929_ccgarchfit2.RData")
-library(shinystan)
-launch_shinystan(ccgarch2)
+save(ccgarch2, file = "20151002_ccgarchfit2.RData")
 
-rstan::monitor(ccgarch2)
 pars <- rstan::extract(ccgarch2, permuted = F)
-
 pars2 <- do.call(rbind, lapply(1:4, function(x) pars[,x,])) %>% as.data.frame
-str(pars2)
 taus <- pars2[,grepl("tau", names(pars2))]
 
 taus <- taus %>% melt %>%
@@ -54,10 +49,13 @@ taus <- taus %>% melt %>%
          series = str_extract(variable, perl("(?<=,)[0-9]{1,3}")) %>% as.numeric)
 taus <- taus %>% group_by(variable) %>%
   summarise(median = median(value),
+            lower = quantile(value, 0.025),
+            upper = quantile(value, 0.075),
             series = first(series),
             observation = first(observation)) %>% filter(!is.na(series))
 
 
 taus %>% ggplot(aes(x = observation, y = median)) +
+  #geom_ribbon(aes(ymin= lower, ymax = upper)) +
   geom_line() +
   facet_grid(series~., scales = "free")
